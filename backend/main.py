4bbdf89d9e6de47f166fc62217b2f2b2ef74a91c
Kaleb Asefa <Kalebasefa0@gmail.com>
2026-07-23 20:31:40 +0300

    feat: initialize FastAPI application with total_listened_time endpoint and add data directory to gitignore

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,9 @@
-def main():
-    print("Hello from backend!")
+from fastapi import FastAPI
 
 
-if __name__ == "__main__":
-    main()
+app = FastAPI()
+
+
+@app.get('/api/total-listened-time')
+def total_listened_time():
+    pass
